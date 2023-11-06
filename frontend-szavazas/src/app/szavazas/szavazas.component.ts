e778a5b5e22371d6b93f38649e2f7a01eeb8aba9
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-11-06 17:54:22 +0100

    Form + submit kész (API része mégnem).

--- frontend-szavazas/src/app/szavazas/szavazas.component.ts
+++ frontend-szavazas/src/app/szavazas/szavazas.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./szavazas.component.css']
 })
 export class SzavazasComponent {
+  minoseg: number = 5;
+  gyorsasag: number = 5;
+  ar = 5;
 
+  onSubmit() {
+    console.log(`Minőség: ${this.minoseg}, Gyorsaság: ${this.gyorsasag},  Ár: ${this.ar}`);
+  }
 }
