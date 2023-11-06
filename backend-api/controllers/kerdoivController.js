11aceba689321824c6fb2bbe2a4cde5a6634aa76
Szücs Zsolt <szucszsolt.hu@gmail.com>
2023-11-06 14:12:39 +0100

    Refaktorálás

--- backend-api/controllers/kerdoivController.js
+++ backend-api/controllers/kerdoivController.js
@@ -7,28 +7,28 @@ const connectMongoDb = require("../config/dbConnection");
 //@access public
 const kerdoivBekuldese = asyncHandler(async (req, res) => {
     clientIP = req.ip;
+    const { minoseg, gyorsasag, ar } = req.body;
+
     if (await Kerdoiv.findOne({ip: clientIP})) {
         res.status(400).json({
             szavazott: true, 
             message: "Ez az IP cím már szavazott"
         });
     }
-
-    const { minoseg, gyorsasag, ar } = req.body;
-    if (!minoseg || !gyorsasag || !ar) {
+    else if (!minoseg || !gyorsasag || !ar) {
         res.status(400).json({
             "message": "Hiányzó adat(ok)!"
         });
     } 
-    
-    if (minoseg < 1 || minoseg > 5 || gyorsasag < 1 || gyorsasag > 5 || ar < 1 || ar > 5) {
+    else if (minoseg < 1 || minoseg > 5 || gyorsasag < 1 || gyorsasag > 5 || ar < 1 || ar > 5) {
         res.status(400).json({
             "message": "Hibás adatok (érvényes érték: 1-5 között))!"
         });
     } 
-    
-    const kerdoiv = await Kerdoiv.create({minoseg, gyorsasag, ar, ip:req.ip});
-    res.status(200).json(kerdoiv);
+    else {
+        const kerdoiv = await Kerdoiv.create({minoseg, gyorsasag, ar, ip:req.ip});
+        res.status(200).json(kerdoiv);
+    }
 });
 
 //@desc Összes kérdőív letöltése
